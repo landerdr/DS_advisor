3a7b67acbbc2e1dfeba230bf4d66b7f662a93cf8
LanderDeRoeck <Lander.DeRoeck@student.uantwerpen.be>
2019-11-11 16:49:53 +0100

    Got to testing part in tutorial

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,13 +1,19 @@
 # services/users/project/config.py
 
+import os
+
+
 class BaseConfig:
     """Base Configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
-    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_TEST_URL")
